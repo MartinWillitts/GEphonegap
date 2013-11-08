8b512b06b411973c91fda558df9f99272cc5276a
Martin Willitts <martin@martinwillitts.com>
2013-11-08 21:49:32 +0000

    just debug alerts

--- www/js/index.js
+++ www/js/index.js
@@ -16,6 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+alert("ra");
 var app = {
     // Application Constructor
     initialize: function() {
@@ -33,17 +34,21 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
+alert("rb");
         app.receivedEvent('deviceready');
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
+alert("rc");
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
 
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
+alert("rd");
         window.location = "index2.html";
+alert("re");
 
         console.log('Received Event: ' + id);
     }
