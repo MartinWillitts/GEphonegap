0cc8f08025099df9bc75c81fa340ca08738f70ed
Martin Willitts <martin@martinwillitts.com>
2013-11-08 20:57:53 +0000

    More

--- www/js/index.js
+++ www/js/index.js
@@ -43,6 +43,7 @@ var app = {
 
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
+        window.location = "index2.html";
 
         console.log('Received Event: ' + id);
     }
